9d21b2080e277582a3ed48db90d52931bb707c12
Guh7004 <gustavochauki2019@gmail.com>
2025-03-04 13:18:00 -0300

    kk

--- AreaProjetcs/Project4/script.js
+++ AreaProjetcs/Project4/script.js
@@ -11,9 +11,22 @@ function search(){
     for (let i = 0; i < contacts.length ;i++){
             if(input.value === contacts[i].name)
             {
+                p.innerHTML = `Contato Encontrado Name:${contacts[i].name} Tel: ${contacts[i].number}`
                 console.log(contacts[i])
-                return
+                break
+            }else{
+                p.innerHTML =  'Contato Não Encontrado'
             }
     }
 }
-    
+function addContact() {
+    let newName = prompt("Digite o nome do contato:");
+    let newNumber = prompt("Digite o número do contato:");
+
+    if (newName && newNumber) {
+        contacts.push({ name: newName, number: newNumber });
+        alert(`Contato ${newName} adicionado com sucesso!`);
+    } else {
+        alert("Nome e número são obrigatórios!");
+    }
+}
