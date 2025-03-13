852b3a26a466f23b11cb30682621751618acc228
Guh7004 <gustavochauki2019@gmail.com>
2025-03-13 15:27:29 -0300

    ConversorComplet

--- AreaProjetcs/MoneyConvert/script.js
+++ AreaProjetcs/MoneyConvert/script.js
@@ -1,43 +1,202 @@
-document.getElementById('moneyInput').addEventListener('input', function (e) {
-    let value = e.target.value;
-
-    value = value.replace(/\D/g, "");
-    if (value === "") {
-        e.target.value = "R$ 0,00";
-        return;
-    }
-    value = (parseFloat(value) / 100).toFixed(2);
-    value = value.replace(".", ",").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-    e.target.value = "R$ " + value;
-});
-
 let valorSelecionado1 = "op1";
 let valorSelecionado2 = "op1";
 
 
 document.getElementById('meuSelect1').addEventListener('change', function () {
     valorSelecionado1 = this.value;
-    console.log("Selecionado:", valorSelecionado1);
 });
 document.getElementById('meuSelect2').addEventListener('change', function () {
     valorSelecionado2 = this.value;
-    console.log("Selecionado:", valorSelecionado2);
+});
+/*Chama BRL sempre que digitar*/
+
+document.getElementById("moneyInput").addEventListener("input", function () {
+    if (total % 2 !== 0) {
+        total += 1;
+    } else {
+        total += 2;
+    }
+    input();
+    if (valorSelecionado1 === "op1") {
+        BRL();
+    }
+    if (valorSelecionado1 === "op2") {
+        USD();
+    }
+    if (valorSelecionado1 === "op3") {
+        EUR();
+    }
 });
 
-function converter() {
-    
-    let valorInput = document.getElementById('moneyInput').value;
+/*Chama BRL sempre que mudar o select*/
+let total = 0;
+document.getElementById("meuSelect1").addEventListener("change", function () {
 
-    valorInput = valorInput.replace("R$", "").replace(/\./g, "").replace(",", ".");
+    /*PAR*/
 
-    let valorNumerico = parseFloat(valorInput);
-    document.getElementById('output1').innerText = "R$" + valorNumerico;
-    if (valorSelecionado1 === "op1" && valorSelecionado2 === "op2") {
-        console.log(valorNumerico * 6);
-        document.getElementById('output2').innerText = "R$" + valorNumerico * 6;
-    }else{
-        console.log(valorNumerico)
+    if (total % 2 !== 0) {
+        total += 1;
+    } else {
+        total += 2;
+    }
+    input();
+
+    if (valorSelecionado1 === "op1") {
+        BRL();
+    }
+    if (valorSelecionado1 === "op2") {
+        USD();
+    }
+    if (valorSelecionado1 === "op3") {
+        EUR();
+    }
+});
+
+/*Chama BRL sempre que mudar o select*/
+
+document.getElementById("meuSelect2").addEventListener("change", function () {
+
+    /*impar*/
+
+    if (total % 2 === 0) {
+        total += 1;
+    } else {
+        total += 2;
+    }
+    input();
+    if (valorSelecionado2 === "op1") {
+        BRL();
+    }
+    if (valorSelecionado2 === "op2") {
+        USD();
+    }
+    if (valorSelecionado2 === "op3") {
+        EUR();
+    }
+});
+function BRL() {
+    const number = document.getElementById("moneyInput").value;
+    let numericValue = parseFloat(number.replace(/\D/g, "")) / 100 || 0;
+    const formatted = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(numericValue);
+    if (total % 2 === 0) {
+        document.getElementById("img1").src = "../../Assets/CoinBRL.svg";
+        document.getElementById("output1").innerText = formatted;
+        moneyInput.value = formatted;
+        
+    } else {
+        document.getElementById("img2").src = "../../Assets/CoinBRL.svg";
+        document.getElementById("output1").innerText = numericValue ;
     }
 }
+function USD() {
+    const number = document.getElementById("moneyInput").value;
+    const numericValue = parseFloat(number.replace(/\D/g, "")) / 100 || 0;
 
+    const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    }).format(numericValue);
+    if (total % 2 === 0) {
+        document.getElementById("img1").src = "../../Assets/CoinUSD.svg";
+        document.getElementById("output1").innerText = formatted;
+        moneyInput.value = formatted;
+        
+    } else {
+        document.getElementById("img2").src = "../../Assets/CoinUSD.svg";
+    }
+}
+function EUR() {
+    const number = document.getElementById("moneyInput").value;
+    const numericValue = parseFloat(number.replace(/\D/g, "")) / 100 || 0;
 
+    const formatted = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+    }).format(numericValue);
+    if (total % 2 === 0) {
+        document.getElementById("img1").src = "../../Assets/CoinEUR.svg";
+        document.getElementById("output1").innerText = formatted;
+        moneyInput.value = formatted;
+        
+    } else {
+        document.getElementById("img2").src = "../../Assets/CoinEUR.svg";
+    }
+}
+function input(){
+    const number = document.getElementById("moneyInput").value;
+    let numericValue = parseFloat(number.replace(/\D/g, "")) / 100 || 0;
+    if(valorSelecionado1 === "op1" && valorSelecionado2 === "op2"){
+        numericValue = numericValue * 0.17;
+        const formatted = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op1" && valorSelecionado2 === "op3"){
+        numericValue = numericValue * 0.16;
+        const formatted = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op2" && valorSelecionado2 === "op1"){
+        numericValue = numericValue * 5.81
+        const formatted = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op2" && valorSelecionado2 === "op3"){
+        numericValue = numericValue * 0.92
+        const formatted = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op3" && valorSelecionado2 === "op1"){
+        numericValue = numericValue * 6.3; 
+        const formatted = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op3" && valorSelecionado2 === "op2"){
+        numericValue = numericValue * 1.09; 
+        const formatted = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op1" && valorSelecionado2 === "op1"){
+        const formatted = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op2" && valorSelecionado2 === "op2"){
+        const formatted = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    if(valorSelecionado1 === "op3" && valorSelecionado2 === "op3"){
+        const formatted = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(numericValue);
+        document.getElementById("output2").innerText = formatted;
+    }
+    
+    
+}
